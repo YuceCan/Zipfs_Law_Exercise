a9be603e8fd0aca4a0340dfe8be8f6f18a675c19
YuceCan <122264630+YuceCan@users.noreply.github.com>
2025-02-08 19:06:00 +0100

    Update 2B.COMPARISON_08.02.2025_Thomas_Can_Yuece_Exercise.py

--- 2B.COMPARISON_08.02.2025_Thomas_Can_Yuece_Exercise.py
+++ 2B.COMPARISON_08.02.2025_Thomas_Can_Yuece_Exercise.py
@@ -5,7 +5,7 @@ import spacy
 
 # Lade spaCy-Modelle
 nlp_en = spacy.load('en_core_web_sm')
-nlp_tr = spacy.load('tr_core_news_md')  # Stelle sicher, dass du das türkische Modell installiert hast
+nlp_tr = spacy.load('tr_core_news_md')
 
 # Verarbeite die Texte
 doc_en = nlp_en("1.  The case originated in an application (no. 11236/09) against the Republic of Turkey lodged with the Court under Article 34 of the Convention for the Protection of Human Rights and Fundamental Freedoms (“the Convention”) by a Turkish national, Mr Mehmet Aytunç Altay (“the applicant”), on 17 February 2006.")
@@ -14,7 +14,7 @@ doc_tr = nlp_tr("1.  Davanın temelinde, Mehmet Aytunç Altay (“başvuran”) 
 def analyze_word_lengths(doc):
     lengths = {}
     for token in doc:
-        # Token muss kein Satzzeichen, kein Leerzeichen, keine Zahl sein und nur Buchstaben enthalten
+       # Token must not be a punctuation, space or number and only contain letters
          if token.is_alpha:
             length = len(token.text)
             if length in lengths:
@@ -23,14 +23,14 @@ def analyze_word_lengths(doc):
                 lengths[length] = 1
     return lengths
 
-# Analysiere Wortlängen
+# Analyse the word lengths:
 lengths_en = analyze_word_lengths(doc_en)
 lengths_tr = analyze_word_lengths(doc_tr)
 
 print("English Word Lengths: ", lengths_en)
 print("Turkish Word Lengths: ", lengths_tr)
 
-# Sortieren
+# Sorting them from the shortest until the largest word length:
 sorted_lengths_en = dict(sorted(lengths_en.items()))
 sorted_lengths_tr = dict(sorted(lengths_tr.items()))
 
